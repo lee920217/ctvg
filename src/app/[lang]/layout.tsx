022095bde79d150f9c5168d88763164c8f598305
lee920217 <francislee920217@gmail.com>
2024-01-17 10:48:03 +0800

    update ogp

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,8 +1,6 @@
 "use client"
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import LanguageContext from './context/LanguageContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +16,13 @@ export default function RootLayout({
 }: RootLayoutProps) {
   return (
       <html lang={params.lang}>
+        <head>
+            <link rel="icon" href="/svg/favicon.svg" sizes="any" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta property="og:title" content="Engineering Tomorrow, Redefining Frontiers" />
+            <meta property="og:description" content="Our services cover the entire software development lifecycle, from consultation, design, development, testing, to operations and community support." />
+            <meta property="og:image" content="/img/ogp.png" />
+        </head>
         <body className={inter.className}>
             {children}
         </body>
